32225b6ca9d017c6b4b604292787d681e0ea915c
Christian Nagel <christiannagel@live.com>
2015-10-19 23:49:33 +0200

    initial commit - Resources - DNX Beta 8

---
+++ Resources/ResourcesSamples/PInvokeSampleLib/FileUtility.cs
@@ -0,0 +1,20 @@
+﻿#if DNX46
+using System.Security.Permissions;
+#endif
+
+namespace PInvokeSampleLib
+{
+
+    public static class FileUtility
+    {
+#if DNX46
+        [FileIOPermission(SecurityAction.LinkDemand, Unrestricted = true)]
+#endif
+        public static void CreateHardLink(string oldFileName,
+                                          string newFileName)
+        {
+            NativeMethods.CreateHardLink(oldFileName, newFileName);
+        }
+    }
+
+}
---
+++ Resources/ResourcesSamples/QuickArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickArray
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+}
