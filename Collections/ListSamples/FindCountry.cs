c3e32ba9844be6e06ba8802018af4a9caa3031b8
Christian Nagel <christian@christiannagel.com>
2015-11-12 07:05:13 +0100

    add searching samples with collections

---
+++ Collections/ListSamples/FindCountry.cs
@@ -0,0 +1,13 @@
+﻿namespace ListSamples
+{
+    public class FindCountry
+    {
+        public FindCountry(string country)
+        {
+            _country = country;
+        }
+        private string _country;
+
+        public bool FindCountryPredicate(Racer racer) => racer?.Country == _country;
+    }
+}
--- Collections/ListSamples/Program.cs
+++ Collections/ListSamples/Program.cs
@@ -3,51 +3,68 @@ using static System.Console;
 
 namespace ListSamples
 {
+
+
+
     public class Program
-  {
-    public void Main()
     {
-      var graham = new Racer(7, "Graham", "Hill", "UK", 14);
-      var emerson = new Racer(13, "Emerson", "Fittipaldi", "Brazil", 14);
-      var mario = new Racer(16, "Mario", "Andretti", "USA", 12);
+        public void Main()
+        {
+            var graham = new Racer(7, "Graham", "Hill", "UK", 14);
+            var emerson = new Racer(13, "Emerson", "Fittipaldi", "Brazil", 14);
+            var mario = new Racer(16, "Mario", "Andretti", "USA", 12);
 
-      var racers = new List<Racer>(20) { graham, emerson, mario };
+            var racers = new List<Racer>(20) { graham, emerson, mario };
 
-      racers.Add(new Racer(24, "Michael", "Schumacher", "Germany", 91));
-      racers.Add(new Racer(27, "Mika", "Hakkinen", "Finland", 20));
+            racers.Add(new Racer(24, "Michael", "Schumacher", "Germany", 91));
+            racers.Add(new Racer(27, "Mika", "Hakkinen", "Finland", 20));
 
-      racers.AddRange(new Racer[] {
+            racers.AddRange(new Racer[] {
                new Racer(14, "Niki", "Lauda", "Austria", 25),
                new Racer(21, "Alain", "Prost", "France", 51)});
 
-      // insert elements
+            // insert elements
 
-      racers.Insert(3, new Racer(6, "Phil", "Hill", "USA", 3));
+            racers.Insert(3, new Racer(6, "Phil", "Hill", "USA", 3));
 
-      // accessing elements
+            // accessing elements
 
-      for (int i = 0; i < racers.Count; i++)
-      {
-        WriteLine(racers[i]);
-      }
+            for (int i = 0; i < racers.Count; i++)
+            {
+                WriteLine(racers[i]);
+            }
 
-      foreach (var r in racers)
-      {
-        WriteLine(r);
-      }
+            foreach (var r in racers)
+            {
+                WriteLine(r);
+            }
 
-      // remove elements
+            // searching
+            int index1 = racers.IndexOf(mario);
+            int index2 = racers.FindIndex(new FindCountry("Finland").FindCountryPredicate);
+            int index3 = racers.FindIndex(r => r.Country == "Finland");
+            Racer racer = racers.Find(r => r.FirstName == "Niki");
+            List<Racer> bigWinners = racers.FindAll(r => r.Wins > 20);
+            foreach (Racer r in bigWinners)
+            {
+                WriteLine($"{r:A}");
+            }
 
-      if (!racers.Remove(graham))
-      {
-        WriteLine("object not found in collection");
-      }
+            WriteLine();
+
+
+            // remove elements
+
+            if (!racers.Remove(graham))
+            {
+                WriteLine("object not found in collection");
+            }
 
 
 
-      var racers2 = new List<Racer>(new Racer[] {
+            var racers2 = new List<Racer>(new Racer[] {
                new Racer(12, "Jochen", "Rindt", "Austria", 6),
                new Racer(22, "Ayrton", "Senna", "Brazil", 41) });
+        }
     }
-  }
 }
