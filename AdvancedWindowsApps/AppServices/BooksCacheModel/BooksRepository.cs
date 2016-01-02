83c75841c149b0d3fd2e4c8addd71d2b5839dabb
Christian Nagel <christian@christiannagel.com>
2016-01-02 21:25:13 +0100

    advanced windows apps - camera sample

---
+++ AdvancedWindowsApps/AppServices/BooksCacheModel/BooksRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BooksCacheModel
+{
+    public class BooksRepository
+    {
+        private readonly List<Book> _books = new List<Book>()
+        {
+            new Book {Title = "Professional C# 6", Publisher = "Wrox Press" }
+        };
+        public IEnumerable<Book> Books => _books;
+
+        private BooksRepository()
+        {
+
+        }
+
+        public static BooksRepository Instance = new BooksRepository();
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+    }
+}
---
+++ AdvancedWindowsApps/CameraSample/CameraSample/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CameraSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public async void OnTakePhoto()
+        {
+            var cam = new CameraCaptureUI();
+            cam.PhotoSettings.AllowCropping = true;
+            cam.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Png;
+            cam.PhotoSettings.CroppedSizeInPixels = new Size(300, 300);
+            StorageFile file = await cam.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (file != null)
+            {
+                var picker = new FileSavePicker();
+                picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                picker.FileTypeChoices.Add("Image File", new string[] { ".png" });
+                StorageFile fileDestination = await picker.PickSaveFileAsync();
+                if (fileDestination != null)
+                {
+                    await file.CopyAndReplaceAsync(fileDestination);
+                }
+            }
+
+        }
+
+        public async void OnRecordVideo()
+        {
+            var cam = new CameraCaptureUI();
+            cam.VideoSettings.AllowTrimming = true;
+            cam.VideoSettings.MaxResolution =
+              CameraCaptureUIMaxVideoResolution.StandardDefinition;
+            cam.VideoSettings.Format = CameraCaptureUIVideoFormat.Wmv;
+            cam.VideoSettings.MaxDurationInSeconds = 5;
+            StorageFile file = await cam.CaptureFileAsync(
+              CameraCaptureUIMode.Video);
+
+            if (file != null)
+            {
+                var picker = new FileSavePicker();
+                picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+                picker.FileTypeChoices.Add("Video File", new string[] { ".wmv" });
+                StorageFile fileDestination = await picker.PickSaveFileAsync();
+                if (fileDestination != null)
+                {
+                    await file.CopyAndReplaceAsync(fileDestination);
+                }
+            }
+        }
+
+    }
+}
