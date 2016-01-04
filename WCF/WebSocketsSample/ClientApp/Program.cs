f9f89ef816a2dfbc6db1666466a104f1cd720be7
Christian Nagel <christian@christiannagel.com>
2016-01-04 10:37:36 +0100

    WCF - WebSockets sample

---
+++ WCF/WebSocketsSample/ClientApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClientApp.DemoService;
+using System.ServiceModel;
+using static System.Console;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        private class CallbackHandler : IDemoServiceCallback
+        {
+            public void SendMessage(string message)
+            {
+                WriteLine($"message from the server {message}");
+            }
+        }
+
+        public static void Main()
+        {
+            WriteLine("client... wait for the server");
+            ReadLine();
+            StartSendRequest();
+            WriteLine("next return to exit");
+            ReadLine();
+            WriteLine("bye...");
+        }
+
+        static async void StartSendRequest()
+        {
+            var callbackInstance = new InstanceContext(new CallbackHandler());
+            var client = new DemoServiceClient(callbackInstance);
+            await client.StartSendingMessagesAsync();
+        }
+    }
+}
---
+++ WCF/WebSocketsSample/WebSocketsSample/DemoService.svc.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace WebSocketsSample
+{
+    public class DemoService : IDemoService
+    {
+
+        public void StartSendingMessages()
+        {
+            IDemoCallback callback = OperationContext.Current.GetCallbackChannel<IDemoCallback>();
+            Task.Run(() => BackgroundSender(callback));
+        }
+
+        private async Task BackgroundSender(IDemoCallback callback)
+        {
+            int loop = 0;
+            while ((callback as IChannel).State == CommunicationState.Opened)
+            {
+                await callback.SendMessage($"Hello from the server {loop++}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
