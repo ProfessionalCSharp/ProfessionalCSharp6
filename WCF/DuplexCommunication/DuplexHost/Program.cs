af3ed89a1cd68613d8a1ce5353a82a9319c1cf61
Christian Nagel <christian@christiannagel.com>
2016-01-04 08:16:07 +0100

    WCF Sample - duplex communication

---
+++ WCF/DuplexCommunication/DuplexHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceModel;
+using static System.Console;
+
+namespace Wrox.ProCSharp.WCF
+{
+    class Program
+   {
+      internal static ServiceHost myServiceHost = null;
+
+      internal static void StartService()
+      {
+         //Instantiate new ServiceHost 
+         myServiceHost = new ServiceHost(typeof(Wrox.ProCSharp.WCF.MessageService));
+
+         //Open myServiceHost
+         myServiceHost.Open();
+      }
+
+      internal static void StopService()
+      {
+         //Call StopService from your shutdown logic (i.e. dispose method)
+         if (myServiceHost.State != CommunicationState.Closed)
+            myServiceHost.Close();
+      }
+
+      static void Main()
+      {
+         StartService();
+         WriteLine("Service running; press return to exit");
+         ReadLine();
+         StopService();
+         WriteLine("stopped");
+      }
+   }
+}
---
+++ WCF/DuplexCommunication/MessageService/MessageService.cs
@@ -0,0 +1,31 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Wrox.ProCSharp.WCF
+{
+    public class MessageService : IMyMessage
+    {
+        public void MessageToServer(string message)
+        {
+            WriteLine($"message from the client: {message}");
+            IMyMessageCallback callback =
+                  OperationContext.Current.
+                        GetCallbackChannel<IMyMessageCallback>();
+
+            callback.OnCallback("message from the server");
+
+            Task.Run(() => TaskCallback(callback));
+        }
+        private async void TaskCallback(object callback)
+        {
+            IMyMessageCallback messageCallback = callback as IMyMessageCallback;
+            for (int i = 0; i < 10; i++)
+            {
+                messageCallback.OnCallback($"message {i}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+}
