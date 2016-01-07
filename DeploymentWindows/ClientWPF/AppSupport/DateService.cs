58da4073e9050d6820470c0c087fa50501b6c766
Christian Nagel <christian@christiannagel.com>
2016-01-07 08:50:30 +0100

    Deployment Windows Apps - WPF

---
+++ DeploymentWindows/ClientWPF/AppSupport/DateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppSupport
+{
+    public class DateService
+    {
+        public string GetLongDateInfoString() => $"Today's date is {DateTime.Today:D}";
+
+
+        public string GetShortDateInfoString() => $"Today's date is {DateTime.Today:d}";
+
+    }
+}
---
+++ DeploymentWindows/ClientWPF/ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using AppSupport;
+using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnGetDate(object sender, RoutedEventArgs e)
+        {
+            var dateService = new DateService();
+            text1.Text = dateService.GetLongDateInfoString();
+            text2.Text = dateService.GetShortDateInfoString();
+        }
+
+        private void OnUpdate(object sender, RoutedEventArgs e)
+        {
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment.CurrentDeployment.CheckForUpdateCompleted +=
+                  (sender1, e1) =>
+                  {
+                      if (e1.UpdateAvailable)
+                      {
+                          ApplicationDeployment.CurrentDeployment.UpdateCompleted +=
+                    (sender2, e2) =>
+                        {
+                            MessageBox.Show("Update completed");
+                        };
+                          ApplicationDeployment.CurrentDeployment.UpdateAsync();
+                      }
+                      else
+                      {
+                          MessageBox.Show("No update available");
+                      }
+
+                  };
+                ApplicationDeployment.CurrentDeployment.CheckForUpdateAsync();
+            }
+            else
+            {
+                MessageBox.Show("not a ClickOnce installation");
+            }
+
+        }
+    }
+}
