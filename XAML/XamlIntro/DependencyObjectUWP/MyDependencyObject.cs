c8fec024821a4e81cce2652689a6f73ea481bc53
Christian Nagel <christian@christiannagel.com>
2016-01-09 19:53:49 +0100

    XAML intro samples

---
+++ XAML/XamlIntro/DependencyObjectUWP/MyDependencyObject.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml;
+
+namespace DependencyObjectUWP
+{
+    public class MyDependencyObject : DependencyObject
+    {
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(MyDependencyObject), new PropertyMetadata(0, OnValueChanged));
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        { 
+            int oldValue = (int)e.OldValue;
+            int newValue = (int)e.NewValue;
+
+        }
+
+        public int Minimum
+        {
+            get { return (int)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register(nameof(Minimum), typeof(int),
+                typeof(MyDependencyObject), new PropertyMetadata(0));
+
+        public int Maximum
+        {
+            get { return (int)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register(nameof(Maximum), typeof(int),
+              typeof(MyDependencyObject), new PropertyMetadata(100));
+
+
+    }
+}
---
+++ XAML/XamlIntro/RoutedEventsUWP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RoutedEventsUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnTappedButton(object sender, TappedRoutedEventArgs e)
+        {
+            ShowStatus(nameof(OnTappedButton), e);
+            e.Handled = CheckStopRouting.IsChecked == true;
+        }
+
+
+        private void OnTappedGrid(object sender, TappedRoutedEventArgs e)
+        {
+            ShowStatus(nameof(OnTappedGrid), e);
+            e.Handled = CheckStopRouting.IsChecked == true;
+        }
+
+        private void ShowStatus(string status, RoutedEventArgs e)
+        {
+            textStatus.Text += $"{status} {e.OriginalSource.GetType().Name}";
+            textStatus.Text += "\r\n";
+        }
+
+        private void OnCleanStatus(object sender, RoutedEventArgs e)
+        {
+            textStatus.Text = string.Empty;
+        }
+    }
+}
