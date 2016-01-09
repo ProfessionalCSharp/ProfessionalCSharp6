7e49ae343f8669ada1b3662f9d943cc772733f69
Christian Nagel <christian@christiannagel.com>
2016-01-09 07:25:01 +0100

    Windows Apps - App Shell sample

---
+++ WindowsApps/AppShellSample/AppShellSample/AppShell.xaml.cs
@@ -0,0 +1,73 @@
+﻿using AppShellSample.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppShellSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            this.InitializeComponent();
+        }
+
+        public Frame AppFrame => frame;
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool handled = false;
+            BackRequested(ref handled);
+        }
+
+        private void BackRequested(ref bool handled)
+        {
+            if (AppFrame?.CanGoBack ?? false && !handled)
+            {
+                handled = true;
+                AppFrame.GoBack();
+            }
+        }
+
+        private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
+        {
+
+        }
+
+        private void OnNavigatedToPage(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void HamburgerMenu_UnChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GoToHomePage()
+        {
+            while (AppFrame?.CanGoBack ?? false) AppFrame.GoBack();
+        }
+
+        public void GoToEditPage()
+        {            
+            AppFrame?.Navigate(typeof(EditPage));
+        }
+    }
+}
---
+++ WindowsApps/AppShellSample/AppShellSample/Converters/BoolToNullableBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace AppShellSample.Converters
+{
+    public class BoolToNullableBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool defaultValue = false;
+            if (parameter != null)
+            {
+                defaultValue = (bool)parameter;
+            }
+
+            bool? val = (bool?)value;
+            return val ?? defaultValue;
+        }
+    }
+}
