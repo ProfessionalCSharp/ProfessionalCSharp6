2d0de641065fcacc1e967d8d53f5e4de649c70e2
Christian Nagel <christian@christiannagel.com>
2016-01-10 12:09:47 +0100

    cleanup, split BooksDemo

--- WPF/WPFSamples/WpfApplication1/Properties/AssemblyInfo.cs
+++ WPF/WPFSamples/WpfApplication1/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BooksDemo")]
+[assembly: AssemblyTitle("WpfApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BooksDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
