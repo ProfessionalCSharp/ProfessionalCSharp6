12f5d4ad3ace22c953e5e5fdd7e8cb5e161fae98
Christian Nagel <christian@christiannagel.com>
2016-01-13 21:22:08 +0100

    MenuPlanner - data sample for ASP.NET MVC

---
+++ ASPNETMVC/MenuPlanner/src/MenuPlanner/Models/MenuCardDatabaseInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuPlanner.Models
+{
+    public class MenuCardDatabaseInitializer
+    {
+        private static bool _databaseChecked = false;
+
+        public MenuCardDatabaseInitializer(MenuCardsContext context)
+        {
+            _context = context;
+        }
+        private MenuCardsContext _context;
+
+        public async Task CreateAndSeedDatabaseAsync()
+        {
+            if (!_databaseChecked)
+            {
+                _databaseChecked = true;
+
+                await _context.Database.MigrateAsync();
+
+                if (_context.MenuCards.Count() == 0)
+                {
+                    _context.MenuCards.Add(
+                      new MenuCard { Name = "Breakfast", Active = true, Order = 1 });
+                    _context.MenuCards.Add(
+                      new MenuCard { Name = "Vegetarian", Active = true, Order = 2 });
+                    _context.MenuCards.Add(
+                      new MenuCard { Name = "Steaks", Active = true, Order = 3 });
+                }
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
