672e159d4b272ade11bd0d32182095727f8c8a75
Christian Nagel <christian@christiannagel.com>
2016-02-04 09:56:14 +0100

    reverse preprocessor directives to make it future proof

--- Security/SigningDemo/Program.cs
+++ Security/SigningDemo/Program.cs
@@ -43,14 +43,13 @@ namespace SigningDemo
             byte[] signature;
             using (var signingAlg = new ECDsaCng(key))
             {
-#if DNXCORE50
-                signature = signingAlg.SignData(data, HashAlgorithmName.SHA512);
-#else
+#if NET46
                 signature = signingAlg.SignData(data);
                 signingAlg.Clear();
+#else
+                signature = signingAlg.SignData(data, HashAlgorithmName.SHA512);
+
 #endif
-
-
             }
             return signature;
         }
@@ -61,11 +60,11 @@ namespace SigningDemo
             using (CngKey key = CngKey.Import(pubKey, CngKeyBlobFormat.GenericPublicBlob))
             using (var signingAlg = new ECDsaCng(key))
             {
-#if DNXCORE50
-                retValue = signingAlg.VerifyData(data, signature, HashAlgorithmName.SHA512);
-#else
+#if NET46
                 retValue = signingAlg.VerifyData(data, signature);
                 signingAlg.Clear();
+#else
+                retValue = signingAlg.VerifyData(data, signature, HashAlgorithmName.SHA512);
 #endif
             }
             return retValue;
