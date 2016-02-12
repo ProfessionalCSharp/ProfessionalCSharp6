777c83d8ec5d3ed5582370de7bd620051af15246
Christian Nagel <christian@christiannagel.com>
2016-02-12 20:32:19 +0100

    add WPF templates for the ListView control

--- StylesAndResources/StylesAndResourcesWPF/ModelsWPF/CountryRepository.cs
+++ StylesAndResources/StylesAndResourcesWPF/ModelsWPF/CountryRepository.cs
@@ -9,10 +9,10 @@ namespace ModelsWPF
     public IEnumerable<Country> GetCountries() =>
       s_countries ?? (s_countries = new List<Country>
       {
-        new Country { Name="Austria", ImagePath = "Images/Austria.bmp" },
-        new Country { Name="Germany", ImagePath = "Images/Germany.bmp" },
-        new Country { Name="Norway", ImagePath = "Images/Norway.bmp" },
-        new Country { Name="USA", ImagePath = "Images/USA.bmp" }
+        new Country { Name="Austria", ImagePath = "/Images/Austria.bmp" },
+        new Country { Name="Germany", ImagePath = "/Images/Germany.bmp" },
+        new Country { Name="Norway", ImagePath = "/Images/Norway.bmp" },
+        new Country { Name="USA", ImagePath = "/Images/USA.bmp" }
       });
   }
 }
