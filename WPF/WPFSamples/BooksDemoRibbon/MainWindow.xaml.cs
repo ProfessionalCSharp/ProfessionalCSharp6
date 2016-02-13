4f775d0d70283fa5950b79310679f3985e09e77a
Christian Nagel <christian@christiannagel.com>
2016-02-13 11:08:47 +0100

    remove not needed namespaces

--- WPF/WPFSamples/BooksDemoRibbon/MainWindow.xaml.cs
+++ WPF/WPFSamples/BooksDemoRibbon/MainWindow.xaml.cs
@@ -1,28 +1,18 @@
 ﻿using BooksDemo.Controls;
 using BooksDemo.Models;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Ribbon;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BooksDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-   public partial class MainWindow : RibbonWindow
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
