955a1e13586a418caabfa7df66b806ed1eef705e
Christian Nagel <christian@christiannagel.com>
2016-02-13 07:09:38 +0100

    remove unneeded code

--- WindowsApps/CompiledBindingSample/CompiledBindingSample/MainPage.xaml.cs
+++ WindowsApps/CompiledBindingSample/CompiledBindingSample/MainPage.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using CompiledBindingSample.Views;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -38,9 +25,5 @@ namespace CompiledBindingSample
             Frame.Navigate(typeof(LifetimeSample));
         }
 
-        public void OnPhasedRendering()
-        {
-            Frame.Navigate(typeof(PhasedRenderingPage));
-       }
     }
 }
