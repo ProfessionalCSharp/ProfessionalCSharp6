c39eab2cda7a82380c660ee9de9f88ce989cd6e1
Christian Nagel <christian@christiannagel.com>
2016-04-09 15:54:43 +0200

    add digit separators and binary literals with C# 7

---
+++ CoreCSharp/Net46CoreCSharpSamples/CoreCSharpSamples/DigitSeparatorSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DigitSeparatorSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // digit separator
+            int n1 = 123_456_789;
+            WriteLine(n1);
+            long n2 = 0x123_4567_89ab_cdef;
+            WriteLine($"{n2:x}");
+
+            // binary literals
+            int b1 = 0b0000_1111_0000;
+            WriteLine(b1);
+            int b2 = 0b1111_1111_1111_1111;
+            WriteLine(b2);
+
+        }
+    }
+}
