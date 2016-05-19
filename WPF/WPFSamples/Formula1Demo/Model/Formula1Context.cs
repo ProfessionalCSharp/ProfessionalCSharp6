3b7d433d4fc6ed00670c5a7fac864792a696677c
Christian Nagel <christian@christiannagel.com>
2016-05-19 23:52:55 +0200

    RC2 updates

--- WPF/WPFSamples/Formula1Demo/Model/Formula1Context.cs
+++ WPF/WPFSamples/Formula1Demo/Model/Formula1Context.cs
@@ -1,26 +1,16 @@
-﻿// using Microsoft.Data.Entity; // uncomment this with EF 7
-using System.Data.Entity;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Formula1Demo.Model
 {
     public class Formula1Context : DbContext
     {
         private const string connectionString = @"server=(localdb)\MSSQLLocalDb;database=Formula1;trusted_connection=true";
-
-        // comment the constructor with EF 7
-        public Formula1Context()
-            : base(connectionString)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            base.OnConfiguring(optionsBuilder);
 
+            optionsBuilder.UseSqlServer(connectionString);
         }
-
-        //// uncomment this with EF 7
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    base.OnConfiguring(optionsBuilder);
-
-        //    optionsBuilder.UseSqlServer(connectionString);
-        //}
         public virtual DbSet<Circuit> Circuits { get; set; }
         public virtual DbSet<Race> Races { get; set; }
         public virtual DbSet<RaceResult> RaceResults { get; set; }
