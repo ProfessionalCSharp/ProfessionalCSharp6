2cb4f7b4a2d5a0231ec596e169c6e30d244a26c6
Christian Nagel <christian@christiannagel.com>
2016-05-27 10:08:43 +0200

    add IDisposable sample to Resources chapter

---
+++ Resources/ResourcesSamples/DisposableSample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DisposableSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var resource = new SomeResource())
+            {
+                resource.Foo();
+            }
+        }
+    }
+}
