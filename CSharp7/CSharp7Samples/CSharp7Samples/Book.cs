0f905377c37dc6776ae74691de4e843a10788dfa
Christian Nagel <christian@christiannagel.com>
2017-03-03 21:30:41 +0100

    C# 7.0 chapter

---
+++ CSharp7/CSharp7Samples/CSharp7Samples/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp7Samples
+{
+    public class Book
+    {
+        public Book(int id, string title, string publisher)
+        {
+            Id = id;
+            Title = title;
+            Publisher = publisher;
+        }
+
+        public int Id { get; }
+        public string Title { get; }
+        public string Publisher { get; }
+
+        public void Deconstruct(out int id, out string title, out string publisher)
+        {
+            id = Id;
+            title = Title;
+            publisher = Publisher;
+        }
+    }
+
+    public static class BookExtensions
+    {
+        public static void Deconstruct(this Book book, out string title, out string publisher) =>
+            book.Deconstruct(out _, out title, out publisher);
+    }
+}
---
+++ CSharp7/CSharp7Samples/CSharp7Samples/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7Samples
+{
+    public class Person
+    {
+        private EventHandler _someEvent;
+        public event EventHandler SomeEvent
+        {
+            add => _someEvent += value;
+            remove => _someEvent -= value;
+        }
+
+        private string _firstName;
+        private string _lastName;
+        public Person(string name) => 
+            name.Split(' ').MoveElementsTo(out _firstName, out _lastName);
+
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+
+        private int _age;
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+
+    }
+
+    public static class PersonExtensions
+    {
+        public static void Deconstruct(this Person p, out string firstname, out string lastname, out int age)
+        {
+            firstname = p.FirstName;
+            lastname = p.LastName;
+            age = p.Age;
+        }
+
+        public static void Deconstruct(this Person p, out string firstname, out string lastname)
+        {
+            firstname = p.FirstName;
+            lastname = p.LastName;
+        }
+    }
+
+    public static class StringCollectionExtension
+    {
+        public static void MoveElementsTo(this IList<string> coll, out string s1, out string s2)
+        {
+            if (coll.Count != 2) throw new ArgumentException("wrong collection count", nameof(coll));
+
+            s1 = coll[0];
+            s2 = coll[1];
+        }
+    }
+}
